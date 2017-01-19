bcee3f7901623fdfa81d41a4e172a410ef57d659
Takatoshi Maeda <me@tmd.tw>
2017-01-19 15:11:58 +0900

    Add all page request mode

--- lib/tembin/redash/query.rb
+++ lib/tembin/redash/query.rb
@@ -3,17 +3,40 @@ require 'diffy'
 
 module Tembin::Redash
   class Query
+    class RequestNotSucceedError < StandardError; end
+
     def self.all
       response = Tembin::Redash::Client.current.get('/api/queries')
       raise RequestNotSucceedError, response.body if !response.success?
       JSON.parse(response.body)['results'].map { |j| self.new(j) }
     end
 
-    def self.created_by_me
-      response = Tembin::Redash::Client.current.get('/api/queries/my')
-      raise RequestNotSucceedError, response.body if !response.success?
-      JSON.parse(response.body)['results'].map { |j| self.new(j) }
+    PAGE_OPTION = {
+      page: 1,
+      page_size: 5,
+    }.freeze
+    def self.created_by_me(fetch_all_page: true)
+      if !fetch_all_page
+        return request_my_query(PAGE_OPTION)['results'].map { |j| self.new(j) }
+      end
+      responses = []
+      response = request_my_query(PAGE_OPTION)
+      responses << response
+      while response['count'] > (response['page'] * response['page_size'])
+        response = request_my_query(page: response['page'] + 1, page_size: response['page_size'])
+        responses << response
+      end
+      responses.flat_map { |r| r['results'].map { |j| new(j) } }
     end
+
+    def self.request_my_query(page_option = {})
+      response = Tembin::Redash::Client.current.get('/api/queries/my', params: page_option)
+      if !response.success?
+        raise RequestNotSucceedError, response.body
+      end
+      JSON.parse(response.body)
+    end
+    private_class_method :request_my_query
 
     INITIAL_DATA_SOURCE_ID = 1
     def self.create(name, sql)
